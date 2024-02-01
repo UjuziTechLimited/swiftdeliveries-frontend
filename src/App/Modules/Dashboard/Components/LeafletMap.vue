9aa9dda6c10b717a61f02d050bdd13b5cca53a27
PeterOwira <92856626+PeterOwira@users.noreply.github.com>
2024-02-01 13:22:51 +0300

    Google maps API key setup

--- src/App/Modules/Dashboard/Components/LeafletMap.vue
+++ src/App/Modules/Dashboard/Components/LeafletMap.vue
@@ -5,16 +5,20 @@ import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 
 
 const zoom = ref(2);
+
+
 </script>
 
 
 <template>
-    <div class="w-full  h-96">
+     <div class="w-full  h-96">
         <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]" :useGlobalLeaflet="false">
             <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap">
             </l-tile-layer>
         </l-map>
-    </div>
+    </div> 
+
+
 </template>
 
 
